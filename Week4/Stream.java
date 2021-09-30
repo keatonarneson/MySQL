2eae2d476f52e27b2e3135063c5868bac12f2ab8
Keaton <keaton.arneson@gmail.com>
2021-09-30 14:01:55 -0500

    Created Stream for step 2

---
+++ Week4/Stream.java
@@ -0,0 +1,18 @@
+package Week4;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Stream {
+  public static void main(String[] args) {
+    List<Animal> animals = Sort.animals;
+    
+    System.out.println(
+      animals.stream()
+      .map(Animal::toString)
+      .sorted()
+      .collect(Collectors.joining(", ")));
+
+
+  }
+}
